ef6c9ce862107af466d26f106205d3f989e5b6b4
user <cyg0828@gmail.com>
2025-07-14 11:23:28 +0900

    feat: 위시리스트 코드 추가

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# spring-gift-enhancement
+# spring-gift-wishlist
+step0
+- wishlist 코드 옮기기
---
+++ src/main/java/gift/controller/ProductController.java
@@ -0,0 +1,44 @@
+package gift.controller;
+
+import gift.dto.ProductRequest;
+import gift.dto.ProductResponse;
+import gift.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductResponse> getAll() {
+        return productService.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse addProduct(@Valid @RequestBody ProductRequest request) {
+        return productService.save(request);
+    }
+
+    @PutMapping("/{id}")
+    public ProductResponse updateProduct(@Valid @PathVariable Long id, @RequestBody ProductRequest request) {
+        return productService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long id) {
+        productService.delete(id);
+    }
+}
+
